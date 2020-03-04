0e2c30a5b3dfd0dd4460e7adc016d664143c138b
WilsonGutierrez <60446487+WilsonGutierrez@users.noreply.github.com>
2020-03-04 14:26:15 -0600

    Entregar

---
+++ Parcial/Parcial/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Parcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.cbotengo = new System.Windows.Forms.ComboBox();
+            this.cboquiero = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblConversion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(55, 13);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(113, 13);
+            this.lblTipo.TabIndex = 0;
+            this.lblTipo.Text = "Seleccione conversor:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(174, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(196, 204);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 2;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            // 
+            // cbotengo
+            // 
+            this.cbotengo.FormattingEnabled = true;
+            this.cbotengo.Location = new System.Drawing.Point(94, 147);
+            this.cbotengo.Name = "cbotengo";
+            this.cbotengo.Size = new System.Drawing.Size(121, 21);
+            this.cbotengo.TabIndex = 4;
+            // 
+            // cboquiero
+            // 
+            this.cboquiero.FormattingEnabled = true;
+            this.cboquiero.Location = new System.Drawing.Point(298, 147);
+            this.cboquiero.Name = "cboquiero";
+            this.cboquiero.Size = new System.Drawing.Size(121, 21);
+            this.cboquiero.TabIndex = 5;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(139, 75);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(43, 78);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(90, 13);
+            this.lblCantidad.TabIndex = 7;
+            this.lblCantidad.Text = "Ingrese Cantidad:";
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Location = new System.Drawing.Point(43, 155);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(24, 13);
+            this.lblDe.TabIndex = 8;
+            this.lblDe.Text = "De:";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(268, 155);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(17, 13);
+            this.lblA.TabIndex = 9;
+            this.lblA.Text = "A:";
+            // 
+            // lblConversion
+            // 
+            this.lblConversion.AutoSize = true;
+            this.lblConversion.Location = new System.Drawing.Point(171, 265);
+            this.lblConversion.Name = "lblConversion";
+            this.lblConversion.Size = new System.Drawing.Size(60, 13);
+            this.lblConversion.TabIndex = 10;
+            this.lblConversion.Text = "Conversión";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 367);
+            this.Controls.Add(this.lblConversion);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cboquiero);
+            this.Controls.Add(this.cbotengo);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblTipo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.ComboBox cbotengo;
+        private System.Windows.Forms.ComboBox cboquiero;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblConversion;
+    }
+}
+
---
+++ Parcial/Parcial/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+
+        }
+
+       class Convertir
+        {
+           public string []tipo = {"Moneda", "Almacenamiento" };
+
+            public string [] [] etiquetas =
+            {
+            new string [] = { "Dólares", "Colones", "Yenes", "Rupia", "Peso (Chileno)", "Peso(Mexicano)", "Peso (Argentino)"},
+            new string [] = { "Megabyte", "Bit", "Byte", "kilobyte", "Gigabyte", "terabyte" },
+
+            };
+
+            public double[][] valores =
+            {
+                new double [] = {1, 8.75, 111.27, 69.75, 667.08, 19.36, 39.69, 0.00026 },
+                new double [] = {1, 8,388,608,  1,048,576, 1,024, 0.0009765625, 0.00000095367431660625 }
+            };
+
+          
+
+    }
+}
